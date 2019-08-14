df401fd6710c07563477e1749ee1df9b24ea04ff
MysticV <leng_vang89@hotmail.com>
2019-08-13 20:23:05 -0700

    Fixed content increasing grid container size

--- src/components/Panel5.js
+++ src/components/Panel5.js
@@ -7,9 +7,11 @@ import React from 'react';
 const Panel5 = () => {
 	return (
 		<div className='esperpanel'>
-			<div className='espername' />
+			<div className='espername'>Diablos</div>
 			<div className='esperstat' />
-			<div className='esperimg' />
+			<div className='esperimg'>
+				<img src='https://gamepedia.cursecdn.com/exvius_gamepedia_en/9/97/Esper-Diabolos-Thumbnail-3.png?version=43c90310dfb12af6c84ae713cdd4091b' alt='failed' />
+			</div>
 			<div className='esperability' />
 
 		</div>
--- src/components/eqsubpanel.js
+++ src/components/eqsubpanel.js
@@ -7,7 +7,7 @@ const EqSubPanel = () => {
 			<div className='eqicon' />
 			<div className='eqstat' />
 			<div className='eqimg'>
-				<img src='' alt='failed' />
+				<img src='https://gamepedia.cursecdn.com/exvius_gamepedia_en/0/08/Icon-Omega_Weapon_%28FFXIII%29.png?version=bad7d1545f58a722eaa2826ff978e82e' alt='failed' />
 			</div>
 			<div className='eqelement' />
 			<div className='eqhandle'>1H
